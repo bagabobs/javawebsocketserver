a1a76f672ee8e109fdb6719de1a7a978d823710c
Baga Gusdiana <baga.gusdiana@len.co.id>
2020-02-16 06:23:32 +0700

    see what client send

--- src/main/java/testwebsocketserver/MainTest.java
+++ src/main/java/testwebsocketserver/MainTest.java
@@ -16,27 +16,32 @@ public class MainTest {
         ServerSocket server = new ServerSocket(8080);
         try {
             System.out.println("Server has started on 127.0.0.1. Waiting for Connection...");
-            Socket client = server.accept();
-            System.out.println("A Client Connected");
-            try(Scanner scanner = new Scanner(client.getInputStream(), "UTF-8");
-                BufferedOutputStream bos = new BufferedOutputStream(client.getOutputStream())) {
-                String data = scanner.useDelimiter("\\r\\n\\r\\n").next();
-                Matcher get = Pattern.compile("^GET").matcher(data);
-                if(get.find()) {
-                    Matcher match = Pattern.compile("Sec-WebSocket-Key: (.*)").matcher(data);
-                    match.find();
-                    byte[] response = ("HTTP/1.1 101 Switching Protocols\r\n" +
-                            "Connection: Upgrade\r\n" +
-                            "Sec-WebSocket-Accept: " +
-                            Base64.getEncoder().encodeToString(MessageDigest.getInstance("SHA-1").digest((match.group(1) + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11").getBytes("UTF-8"))) +
-                            "\r\n\r\n").getBytes("UTF-8");
-                    bos.write(response);
-                    bos.flush();
+            try(Socket client = server.accept()) {
+                System.out.println("A Client Connected");
+                try (Scanner scanner = new Scanner(client.getInputStream(), "UTF-8");
+                     BufferedOutputStream bos = new BufferedOutputStream(client.getOutputStream())) {
+                    String data = scanner.useDelimiter("\\r\\n\\r\\n").next();
+                    Matcher get = Pattern.compile("^GET").matcher(data);
+                    if (get.find()) {
+                        Matcher match = Pattern.compile("Sec-WebSocket-Key: (.*)").matcher(data);
+                        match.find();
+                        byte[] response = ("HTTP/1.1 101 Switching Protocols\r\n" +
+                                "Connection: Upgrade\r\n" +
+                                "Sec-WebSocket-Accept: " +
+                                Base64.getEncoder().encodeToString(MessageDigest.getInstance("SHA-1").digest((match.group(1) + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11").getBytes("UTF-8"))) +
+                                "\r\n\r\n").getBytes("UTF-8");
+                        bos.write(response);
+                        bos.flush();
 
+                        data = scanner.useDelimiter("\\r\\n\\r\\n").next();
+                        System.out.println(data);
+                    }
 
+                } catch (IOException ex) {
+                    throw ex;
                 }
-            } catch(IOException ex) {
-                throw ex;
+            } catch(IOException ez) {
+                throw ez;
             }
         }
         catch(Exception e) {
